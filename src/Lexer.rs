ddbf5ac41361b86f37a552f1a19b4de982f2a034
Richard <rcoric03@gmail.com>
2023-03-05 01:42:36 +0000

    Finish Lexer and Implement Basic Operations

--- src/Lexer.rs
+++ src/Lexer.rs
@@ -1,4 +1,5 @@
-enum TokenType {
+#[derive(Debug)]
+pub enum Intrinsic {
     ShiftLeft,
     ShiftRight,
     Increment,
@@ -9,11 +10,25 @@ enum TokenType {
     JumpBack,
 }
 
+#[derive(Debug)]
+pub struct Token {
+    pub intrinsic: Intrinsic,
+    position: usize,
+}
+
 pub struct Tokenizer {
     content: String,
     content_length: i32,
     cursor: usize,
+    line: i32,
+    column: i32,
 }
+
+fn report_error(token: char, line: i32, col: i32) {
+    println!("Unexpected token: {token} found at {line}:{col}");
+    panic!("Unexpected Token");
+}
+
 
 impl Tokenizer {
     pub fn new(content: &String, content_length: i32) -> Self {
@@ -21,20 +36,101 @@ impl Tokenizer {
             content: content.to_string(), 
             content_length,
             cursor: 0,
+            line: 1,
+            column: 1,
         }
     }
 
-    pub fn next_token(&mut self) -> Option<char> {
+    fn next_lexeme(&mut self) -> Option<char> {
         if self.cursor < self.content_length.try_into().unwrap() {
             let scontent = self.content.to_string();
             let bcontent = scontent.as_bytes();
             let symbol: char = bcontent[self.cursor] as char; 
             self.cursor += 1;
+            
+            // skipping escape characters
+            if symbol == '\n' || symbol == '\r' || symbol == '\t' {
+                self.column = 1;
+                self.line += 1;
+                return None;
+            }
+           
+            // skipping lines with a comment.
+            if self.column == 1 && symbol.is_alphabetic() {
+                let mut s: char = bcontent[self.cursor] as char;
+                while s != '\n'{
+                    self.cursor += 1;
+                    s = bcontent[self.cursor] as char;
+                }
+                return None;
+            }
+
+            self.column += 1;
+
             return Some(symbol)
         }
         else {
             return None
         }
     }
-}
 
+   fn tokenize_single_character(&mut self, lexeme: Option<char>) -> Option<Token> {
+        match lexeme {
+            Some('+') => {
+                let token = Token { intrinsic: Intrinsic::Increment, position: self.cursor };
+                return Some(token);
+            },
+            Some('-') => {
+                let token = Token { intrinsic: Intrinsic::Decrement, position: self.cursor };
+                return Some(token);
+            },
+            Some('>') => {
+                let token = Token { intrinsic: Intrinsic::ShiftRight, position: self.cursor };
+                return Some(token);
+            },
+            Some('<') => {
+                let token = Token { intrinsic: Intrinsic::ShiftLeft, position: self.cursor };
+                return Some(token);
+            },
+            Some('[') => {
+                let token = Token { intrinsic: Intrinsic::JumpOver, position: self.cursor };
+                return Some(token);
+            },
+            Some(']') => {
+                let token = Token { intrinsic: Intrinsic::JumpBack, position: self.cursor };
+                return Some(token);
+            },
+            Some(',') => {
+                let token = Token { intrinsic: Intrinsic::Input, position: self.cursor };
+                return Some(token);
+            },
+            Some('.') => {
+                let token = Token { intrinsic: Intrinsic::Output, position: self.cursor };
+                return Some(token);
+            },
+            Some(x) => {
+                report_error(x, self.line, self.column);
+                return None;
+            },
+            None => {
+                return None;
+            },
+        }   
+   }
+
+    pub fn tokenize(&mut self) -> Vec<Token> {
+        let mut tokens: Vec<Token> = vec![];
+        
+        while self.cursor < self.content_length.try_into().unwrap() {
+            let lexeme: Option<char> = self.next_lexeme();
+            let token: Option<Token> = self.tokenize_single_character(lexeme);
+            match token {
+                Some(t) => {
+                    tokens.push(t);
+                },
+                None => (),
+            }
+        } 
+        return tokens;
+    } 
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 // https://dgtools.readthedocs.io/en/latest/brainfuck.html
-use Lexer::Tokenizer;
+use lexer::Tokenizer;
+use lexer::Intrinsic;
 use std::env;
 use std::fs;
+use std::io;
 
-pub mod Lexer;
+pub mod lexer;
+
+const MAX_SIZE: usize = 1000;
 
 fn lex_file(file_path: &String) -> Tokenizer{
     let contents = fs::read_to_string(file_path)
@@ -15,6 +19,84 @@ fn lex_file(file_path: &String) -> Tokenizer{
 }
 
 
+fn interpret(program: &Vec<lexer::Token>) {
+    let mut tape: [i32; MAX_SIZE] = [0; MAX_SIZE];
+    let mut pointer: usize = 0;
+    let mut program_counter: i32 = 0;
+    let mut stdout: Vec<String> = vec![];
+
+    while program_counter < program.len().try_into().unwrap() {
+        let ptr = program_counter as usize;
+        let instruction: &lexer::Token = &program[ptr];
+        match instruction.intrinsic {
+            Intrinsic::Increment  => {
+                if tape[pointer] > 255 {
+                    tape[pointer] = 0;
+                } else{
+                    tape[pointer] += 1;   
+                }
+            },
+
+            Intrinsic::Decrement  => {
+                if tape[pointer] < 1 {
+                    tape[pointer] = 255;
+                } else {
+                    tape[pointer] -= 1;
+                }
+            },
+
+            Intrinsic::ShiftLeft  => {
+                if pointer < 1 {
+                    pointer = 0;
+                } else {
+                    pointer -= 1;
+                }
+            },
+
+            Intrinsic::ShiftRight => {
+                if pointer > MAX_SIZE.try_into().unwrap() {
+                    pointer = MAX_SIZE - 1;
+                } else {
+                    pointer += 1;
+                }
+            },
+
+            Intrinsic::Input      => {
+                let mut userInput = String::new();
+                
+                io::stdin()
+                    .read_line(&mut userInput)
+                    .unwrap();
+                let parsedInput = userInput.trim();
+
+                match parsedInput.parse::<char>() {
+                    Ok(i) => {
+                        tape[pointer] = i as i32;
+                    },
+                    Err(..) => println!("Invalid Input please enter a character."),
+                }
+            },
+
+            Intrinsic::Output     => {
+                let ascii = char::from_u32(tape[pointer].try_into().unwrap()); 
+                match ascii {
+                    Some(x) => stdout.push(x.to_string()),
+                    None => print!(""),
+                }
+            },
+
+            Intrinsic::JumpOver   => {
+                todo!();
+            },
+
+            Intrinsic::JumpBack   => {
+                todo!();   
+            },
+        }
+        program_counter += 1;
+    }
+    println!("{}", stdout.join(""));
+} 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -24,9 +106,9 @@ fn main() {
         panic!("File Path not Provided...");
     }
     let mut lexer: Tokenizer = lex_file(&args[1]);
-    let c = lexer.next_token();
-    let cn = lexer.next_token();
-    println!("{:#?}", c);
-    println!("{:#?}", cn);
+    let token_vector: Vec<lexer::Token> = lexer.tokenize();
+        
+    interpret(&token_vector);
+
     println!("Hello, world!");
 }
